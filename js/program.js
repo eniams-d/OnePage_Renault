99517b31b302f4079eced37f0b44c3c96f04c00a
eniams-d <djebbari.smain@yahoo.fr>
2018-11-29 16:01:06 +0100

    debug

--- js/program.js
+++ js/program.js
@@ -1,27 +1,31 @@
-
-
-
-    document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 
     //TITLE
     const title = document.querySelector('.slogan h1');
-    const texts = Array("Le design au service de la performance","La sportivité à l'état pur","La perfection dans les détails");
+    const texts = Array('Le design au service de la performance','La sportivité à l\'état pur',"La perfection dans les détails");
     const textsLength = texts.length;
     var currentText = 0;
+
     function changeText() {
+
         setTimeout(function () {
             title.classList.remove('leave');
             title.innerHTML = texts[currentText];
             title.classList.add('enter');
+
             setTimeout(function () {
                 title.classList.remove('enter');
                 title.classList.add('leave');
                 currentText++;
+
                 if(currentText > textsLength-1) {
                     currentText = 0;
                 }
+
                 changeText();
+
             }, 7000)
+            
         }, 1000)
     }
     changeText();
