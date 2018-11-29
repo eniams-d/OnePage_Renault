66596deb26af60130a54b3f51f24043c33eac9da
eniams-d <djebbari.smain@yahoo.fr>
2018-11-29 15:20:51 +0100

    debug

--- js/program.js
+++ js/program.js
@@ -1,39 +1,36 @@
-// charge le DOM
+ // charge le DOM
+    document.addEventListener("DOMContentLoaded", function () {
+ 
+      const title = document.querySelector(".slogan h1"); // selectionne me H1
+      const texts = title.getAttribute("data-text").split("/"); // selectionne le texte inscris dans data-text et transforme et le coupe à chaque "/"
+      const textsLength = texts.length; // récupère la taille du texte
+      var currentText = 0; // indice du premier bout de texte
 
-document.addEventListener('DOMContentLoaded', function () {
-	const title = document.querySelector(".slogan h1");
-	const texts = title.getAttribute("data-text").split("/");
-	const textLength = texts.length;
-	var currentText =0;
+      // fonction qui va faire défiler le texte
+      function changeText() {
 
+        setTimeout(function () { // créer une action infini sur un interval de 1 seconde
 
-	function changeText(){
+          title.classList.remove('leave'); // récupère la class "leave" et la retire
+          title.innerHTML = texts[currentText]; // associe le bout de texte à son indice ici => texts[0]
+          title.classList.add('enter'); //Ajoute la class "enter"
 
-		setTimeout(function(){
+          setTimeout(function () { // créer une action infini sur un interval de 7 seconde
 
-			title.classList.remove('leave');
-			title.innerHTML = texts[currentText];
-			title.classList.add('enter');
+            title.classList.remove('enter');// retire la class "enter"
+            title.classList.add('leave'); // ajoute la class 'leave'
+            currentText++; // incrémente l'indice exemple currenText = 0 puis 1 puis 2 puis 3... 
+            if (currentText > textsLength - 1) { // condition si currenText devient supérieur à la taille du text entier
+              currentText = 0; // Alors on réinitialise currentText à 0
+            }
 
+            changeText(); // appelle de la fonction en local
 
-		setTimeout(function(){
+          }, 7000)
 
-			title.classList.remove('enter');
-			title.classList.add('leave');
-			currentText++;
-			if(currentText > textsLength -1) {
+        }, 1000)
 
-				currentText = 0;
-			}
+      }
 
-			changeText();
-		}, 7000)
-
-		}, 1000)
-
-	}
-
-
-	changeText();
-
-});
+      changeText(); // appelle de la fonction global
+    });
