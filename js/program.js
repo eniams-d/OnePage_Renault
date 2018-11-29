5fe095084c34fe109953f3a0f8b21c0047a664c9
eniams-d <djebbari.smain@yahoo.fr>
2018-11-29 15:06:27 +0100

    ajout du scrip js

---
+++ js/program.js
@@ -0,0 +1,39 @@
+// charge le DOM
+document.addEventlistener("DOMContentLoaded", function() {
+
+	const title = document.querySelector(".slogan h1");
+	const texts = title.getAttribute("data-text").split("/");
+	const textLength = texts.length;
+	var currentText =0;
+
+
+	function changeText(){
+
+		setTimeout(function(){
+
+			title.classList.remove('leave');
+			title.innerHTML = texts[currentText];
+			title.classList.add('enter');
+
+
+		setTimeout(function(){
+
+			title.classList.remove('enter');
+			title.classList.add('leave');
+			currentText++;
+			if(currentText > textsLength -1) {
+
+				currentText = 0;
+			}
+
+			changeText();
+		}, 7000)
+
+		}, 1000)
+
+	}
+
+
+	changeText();
+
+});
